b80aa7f89824e731f46be2a1aa63bc96ce8c6e10
Bryan Miller <bmille12@caledonian.ac.uk>
2012-09-13 10:01:01 +0100

    Changed

---
+++ GP2/main.cpp
@@ -0,0 +1,31 @@
+//Are we on the windows platform?
+#ifdef WIN32
+//if so include the header for windows applications
+#include <Windows.h>
+#endif
+
+#include "Win32Window.h"
+
+//Check to see if we are on windows
+#ifdef WIN32
+int WINAPI WinMain(HINSTANCE hInstance, 
+					HINSTANCE hPrevInstance, 
+					LPSTR lpCmdLine,
+					int nCmdShow)
+#else
+//we are on some other platform
+int main(int argc, char **argv)
+#endif
+{
+	CWin32Window * pWin=new CWin32Window();
+	pWin->init(TEXT("Hello World"),860,640,false);
+
+	while(pWin->running()){
+		pWin->checkForWindowMessages();
+	}
+
+	delete pWin;
+
+	return 0;
+}
+
